78043a3380caeccd977e63e0f8ca41175ecced6c
CASASCASTDIM <mercadotecnia@castdim.mx>
2026-08-24 12:57:13 -0600

    Delete api directory

--- api/config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Copia este archivo como config.php y coloca tus credenciales reales.
-// config.php NUNCA debe compartirse ni subirse a un repositorio público.
-
-define('MONDAY_API_TOKEN', 'PEGA_AQUI_TU_TOKEN_DE_MONDAY');
-define('MONDAY_BOARD_ID', 0);
-define('CACHE_TTL_SECONDS', 300);
